b2f20ee44e361c778b61d18eeffbf98d95e68128
gbmao <gabriel.machado.oli@gmail.com>
2025-08-16 13:35:38 -0300

    wip: calculadora com operadores simples

---
+++ src/main/java/Calculator/Calculadora.java
@@ -0,0 +1,46 @@
+package Calculator;
+
+import java.util.Scanner;
+//TODO call class in a loop for continuous calculations
+public class Calculadora {
+
+    public static int calculator(){
+        Scanner scanner = new Scanner(System.in);
+        //TODO check for more complexity
+        String input = scanner.nextLine().trim().replace(" ", "");
+
+
+        String[] operators = {"*", "+", "-", "/"};
+        String[] equation = input.split("\\D+"); // \\D+ is working , but I don't feel confident with that
+        int first = Integer.parseInt(equation[0]);
+        int last = Integer.parseInt(equation[1]);
+
+        //TODO create methods for each operator
+        for (String operator : operators) {
+
+            if (input.contains(operator)) {
+
+                switch (operator) {
+                    case "*" -> {
+                        return first * last;
+                    }
+                    case "+" -> {
+                        return first + last;
+                    }
+                    case "-" -> {
+                        return first - last;
+                    }
+                    case "/" -> {
+                        return first / last;
+
+                    }
+                    default -> {
+                        return -1;
+                    }
+                }
+            }
+
+        }
+        return 666;
+    }
+}
---
+++ src/main/java/Calculator/Main.java
@@ -0,0 +1,9 @@
+package Calculator;
+
+public class Main {
+    public static void main(String[] args){
+
+//    Calculadora.calculator();
+        System.out.println(Calculadora.calculator());
+    }
+}
